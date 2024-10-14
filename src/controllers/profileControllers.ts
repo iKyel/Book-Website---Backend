1c1f43c039e27ffe0327b90d7174995b2c57d1ca
IAmBiny <anhtruong100703@gmail.com>
2024-10-14 16:35:26 +0700

    update status code and add route 'changePassword'

--- src/controllers/profileControllers.ts
+++ src/controllers/profileControllers.ts
@@ -1,18 +1,46 @@
 import { Request, Response } from "express";
 import { UserModel } from "../models/UserModel.js";
+import bcrypt, { hash } from "bcrypt";
 
+// Get profile
 const getProfileUser = async (req: Request, res: Response) => {
     const { userName } = req.body;
     try {
         const user = await UserModel.findOne({ userName: userName }).exec();
         if (user) {
-            res.status(200).json({ message: "Success", user: user });
+            res.status(200).json({ message: 'Profile retrieved successfully', user: user });
         } else {
-            res.status(400).json({ message: "Failure" });
+            res.status(404).json({ message: 'User not found' });
         }
     } catch (err) {
-        res.status(500).json({ error: err });
+        console.log(err)
+        res.status(500).json({ error: 'Internal server error' });
     }
 }
 
-export { getProfileUser };
+// update password
+const changePassword = async (req: Request, res: Response) => {
+    const { userName, newPassword } = req.body;
+    try {
+        const user = await UserModel.findOne({ userName: userName }).exec();
+        if (user) {
+            // Hash the new password before updating
+            bcrypt.hash(newPassword, 10, async (err, hashedPassword) => {
+                if (err) throw err;
+                const updatedUser = await UserModel.findOneAndUpdate({ userName }, { password: hashedPassword });
+                if (updatedUser) {
+                    res.status(200).json({ message: 'Password updated successfully', user: updatedUser });
+                } else {
+                    res.status(500).json({ message: 'Failed to update password' });
+                }
+            });
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+export { getProfileUser, changePassword };
