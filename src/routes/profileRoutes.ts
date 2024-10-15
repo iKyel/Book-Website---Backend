45871f439ccfab9a20c9882234e1187b9effaa6c
IAmBiny <anhtruong100703@gmail.com>
2024-10-15 22:20:42 +0700

    add authenticate in '/profile' routes

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { changePassword, getProfileUser } from "../controllers/profileControllers.js";
+import authenticate from "../middlewares/authenticateToken.js";
 
 export const profileRouter = express.Router();
 
-profileRouter.post('/getProfile', getProfileUser);
+profileRouter.post('/getProfile', authenticate, getProfileUser);
 
-profileRouter.put('/changePassword', changePassword);
+profileRouter.put('/changePassword', authenticate, changePassword);
