d3dc12bb0b5cdfa6359af2c2a054f46d697ebcf1
IAmBiny <anhtruong100703@gmail.com>
2024-10-19 11:50:26 +0700

    fix conflict trong 'profileRoutes.ts'

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -4,13 +4,8 @@ import authenticate from "../middlewares/authenticateToken.js";
 
 export const profileRouter = express.Router();
 
-<<<<<<< HEAD
-profileRouter.post('/getProfile', authenticate, getProfileUser);
-
-=======
 // Route lấy thông tin user
 profileRouter.get('/getProfile', authenticate, getProfileUser);
 
 // Route sửa mật khẩu
->>>>>>> feature/loc-timkiem-sapxep
 profileRouter.put('/changePassword', authenticate, changePassword);
