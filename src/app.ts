ede06fe43c594b91c5cf7f52c36d084e011b6960
Do Xuan Kien <111947701+iKyel@users.noreply.github.com>
2024-11-22 23:40:42 +0700

    update origin

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(cookieParser());    // Parsing cookie form request
 
 // Middleware for handling CORS Policy
 app.use(cors({
-    origin: 'https://book-website-frontend.vercel.app',
+    origin: 'https://www.booknext.store/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
     credentials: true
